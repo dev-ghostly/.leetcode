fa00b7ccb067cc74fd4324f9f9805fb7ec2af48b
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-04-16 13:07:53 +0200

    challenge(69): done

---
+++ 69.sqrt-x.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=69 lang=javascript
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    const result = Math.sqrt(x);
+    return Math.floor(result);
+};
+// @lc code=end
+
